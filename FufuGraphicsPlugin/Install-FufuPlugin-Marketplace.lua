cbdf5c11bfea8d4cb423866dabb96a755fb8ab0b
シリアCelia <celia@localhost>
2026-08-21 17:52:00 +0800

    release: prepare v1.2.3

--- FufuGraphicsPlugin/Install-FufuPlugin-Marketplace.lua
+++ FufuGraphicsPlugin/Install-FufuPlugin-Marketplace.lua
@@ -118,7 +118,7 @@ install.write_config(plugin_dir, {
         Description = "支持把原神的FSR2转换为FSR4、DLSS、XESS",
         Developer = "シリアCelia",
         File = "FSR-Bridge-Plugin.dll",
-        Version = "1.2.2"
+        Version = "1.2.3"
     },
     EnableBridge = {
         Name = "启用 FSR Bridge",
